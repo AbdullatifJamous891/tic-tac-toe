6596d182b9b9b60276d629a80ad629ad81915d4b
abdullatifJamous891 <abdullatif.jamous891@gmail.com>
2020-09-22 08:01:04 +0300

    computerHit function

--- index.js
+++ index.js
@@ -16,11 +16,33 @@ const playerHit = (element_id)=> {
 		document.getElementById('winner').style.display = 'block';
 
 	}
+	else {
+		var computer_hit = computerHit()
+		console.log(computer_hit)
+		var boxes_obj = computer_hit[0]
+		var isWinner = computer_hit[1]['isWinner']
+
+		if(isWinner){
+			for(var Id in boxes_obj){
+				document.getElementById(Id).style.backgroundColor = '#ff00ff';
+				document.getElementById('winner').textContent = 'Winner: O'
+				document.getElementById('winner').style.display = 'block';
+			}
+		}
+		
+		for(var Id in boxes_obj){
+			if(boxes_obj[Id] == ''){
+				document.getElementById(Id).textContent = 'O';
+				return;
+			}
+		}
+	}
 }
 
 const replay = ()=> {
 	location.reload();
 }
+
 
 const checkWinner = ()=> {
 	var box1 = document.getElementById("box1").textContent;
@@ -65,7 +87,146 @@ const checkWinner = ()=> {
 	else {
 		return {}
 	}
+}
 
+
+const computerHit = ()=> {
+	
+	var box1 = document.getElementById("box1").textContent;
+	var box2 = document.getElementById("box2").textContent;
+	var box3 = document.getElementById("box3").textContent;
+
+	var box4 = document.getElementById("box4").textContent;
+	var box5 = document.getElementById("box5").textContent;
+	var box6 = document.getElementById("box6").textContent;
+
+	var box7 = document.getElementById("box7").textContent;
+	var box8 = document.getElementById("box8").textContent;
+	var box9 = document.getElementById("box9").textContent;
+
+	var row1 = box1+box2+box3;
+	var row2 = box4+box5+box6;
+	var row3 = box7+box8+box9;
+
+	var col1 = box1+box4+box7;
+	var col2 = box2+box5+box8;
+	var col3 = box3+box6+box9;
+
+	var diag1 = box1+box5+box9;
+	var diag2 = box3+box5+box7;
+
+	//check a chance to winner
+	//check rows
+	if(row1 === 'OO'){
+		return [{ 'box1': box1, 'box2': box2, 'box3': box3}, {'isWinner': true }]		 
+	}
+	else if(row2 == 'OO'){
+		return [{ 'box4': box4, 'box5': box5, 'box6': box6}, {'isWinner': true }]
+	}
+	else if(row3 == 'OO'){
+		return [{ 'box7': box7, 'box8': box8, 'box9': box9}, {'isWinner': true }]
+	}
+	//check columns
+	else if(col1 == 'OO'){
+		return [{ 'box1': box1, 'box4': box4, 'box7': box7}, {'isWinner': true }]
+	}
+	else if(col2 == 'OO'){
+		return [{ 'box2': box2, 'box5': box5, 'box8': box8}, {'isWinner': true }]
+	}
+	else if(col3 == 'OO'){
+		return [{ 'box3': box3, 'box6': box6, 'box9': box9}, {'isWinner': true }]
+	}
+	//check diagonal
+	else if(diag1 == 'OO'){
+		return [{ 'box1': box1, 'box5': box5, 'box9': box9}, {'isWinner': true }]
+	}
+	else if(diag2 == 'OO'){
+		return [{ 'box3': box3, 'box5': box5, 'box7': box7}, {'isWinner': true }]
+	}
+
+	//check to prevent player be winner
+	if(row1 === 'XX'){
+		return [{ 'box1': box1, 'box2': box2, 'box3': box3}, {'isWinner': false }]		 
+	}
+	else if(row2 == 'XX'){
+		return [{ 'box4': box4, 'box5': box5, 'box6': box6}, {'isWinner': false }]
+	}
+	else if(row3 == 'XX'){
+		return [{ 'box7': box7, 'box8': box8, 'box9': box9}, {'isWinner': false }]
+	}
+	//check columns
+	else if(col1 == 'XX'){
+		return [{ 'box1': box1, 'box4': box4, 'box7': box7}, {'isWinner': false }]
+	}
+	else if(col2 == 'XX'){
+		return [{ 'box2': box2, 'box5': box5, 'box8': box8}, {'isWinner': false }]
+	}
+	else if(col3 == 'XX'){
+		return [{ 'box3': box3, 'box6': box6, 'box9': box9}, {'isWinner': false }]
+	}
+	//check diagonal
+	else if(diag1 == 'XX'){
+		return [{ 'box1': box1, 'box5': box5, 'box9': box9}, {'isWinner': false }]
+	}
+	else if(diag2 == 'XX'){
+		return [{ 'box3': box3, 'box5': box5, 'box7': box7}, {'isWinner': false }]
+	}
+
+	//check the beter place
+	if(row1 === 'O'){
+		return [{ 'box1': box1, 'box2': box2, 'box3': box3}, {'isWinner': false }]		 
+	}
+	else if(row2 == 'O'){
+		return [{ 'box4': box4, 'box5': box5, 'box6': box6}, {'isWinner': false }]
+	}
+	else if(row3 == 'O'){
+		return [{ 'box7': box7, 'box8': box8, 'box9': box9}, {'isWinner': false }]
+	}
+	//check columns
+	else if(col1 == 'O'){
+		return [{ 'box1': box1, 'box4': box4, 'box7': box7}, {'isWinner': false }]
+	}
+	else if(col2 == 'O'){
+		return [{ 'box2': box2, 'box5': box5, 'box8': box8}, {'isWinner': false }]
+	}
+	else if(col3 == 'O'){
+		return [{ 'box3': box3, 'box6': box6, 'box9': box9}, {'isWinner': false }]
+	}
+	//check diagonal
+	else if(diag1 == 'O'){
+		return [{ 'box1': box1, 'box5': box5, 'box9': box9}, {'isWinner': false }]
+	}
+	else if(diag2 == 'O'){
+		return [{ 'box3': box3, 'box5': box5, 'box7': box7}, {'isWinner': false }]
+	}
+
+	//other choices
+	if(row1 === ''){
+		return [{ 'box1': box1, 'box2': box2, 'box3': box3}, {'isWinner': false }]		 
+	}
+	else if(row2 == ''){
+		return [{ 'box4': box4, 'box5': box5, 'box6': box6}, {'isWinner': false }]
+	}
+	else if(row3 == ''){
+		return [{ 'box7': box7, 'box8': box8, 'box9': box9}, {'isWinner': false }]
+	}
+	//check columns
+	else if(col1 == ''){
+		return [{ 'box1': box1, 'box4': box4, 'box7': box7}, {'isWinner': false }]
+	}
+	else if(col2 == ''){
+		return [{ 'box2': box2, 'box5': box5, 'box8': box8}, {'isWinner': false }]
+	}
+	else if(col3 == ''){
+		return [{ 'box3': box3, 'box6': box6, 'box9': box9}, {'isWinner': false }]
+	}
+	//check diagonal
+	else if(diag1 == ''){
+		return [{ 'box1': box1, 'box5': box5, 'box9': box9}, {'isWinner': false }]
+	}
+	else if(diag2 == ''){
+		return [{ 'box3': box3, 'box5': box5, 'box7': box7}, {'isWinner': false }]
+	}
 
 
 }
